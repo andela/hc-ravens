42e0d4169af6114e1ee5d7d45084ad1c3e0751df
kevinbett <bettkevin757@gmail.com>
2018-04-24 18:02:59 +0300

    feat(tests):Adds switch_team test

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -15,6 +15,8 @@ class SwitchTeamTestCase(BaseTestCase):
 
         ### Assert the contents of r
 
+        self.assertEqual(r.status_code, 200)
+
 
     def test_it_checks_team_membership(self):
         self.client.login(username="charlie@example.org", password="password")
@@ -22,6 +24,7 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url)
         ### Assert the expected error code
+        self.assertEqual(r.status_code, 403)
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
@@ -29,3 +32,4 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url, follow=True)
         ### Assert the expected error code
+        self.assertEqual(r.status_code, 200)
