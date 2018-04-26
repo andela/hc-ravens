def00e0c73f27742b558c9817ad4bffbbb5146e5
kevinbett <bettkevin757@gmail.com>
2018-04-26 12:42:29 +0300

    Amend(tests):Ammends sendalert tests. Reverses list order

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -25,8 +25,10 @@ class SendAlertsTestCase(BaseTestCase):
 
         handled_names = []
         for args, kwargs in mock.call_args_list:
-            handled_names.append(args[0].name)    
+            handled_names.append(args[0].name)
+        handled_names.reverse()
         self.assertEqual(names, handled_names)
+
         ### The above assert fails. Make it pass
 
     def test_it_handles_grace_period(self):
